f970ca9f17eda4cdd28febbadeed4a7eeefe20da
Pedro Pla <ppla2001@gmail.com>
2020-11-19 20:48:39 -0300

    Arregle buscador

--- JS/resultadoBusqueda.js
+++ JS/resultadoBusqueda.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
     var queryString = location.search;
     var queryStringObject = new URLSearchParams(queryString);
     var pedidoUsuario = queryStringObject.get('query');
-    
+
     fetch (`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${pedidoUsuario}&page=1&include_adult=false`)
     .then(function(response){
         return response.json()
@@ -11,60 +11,50 @@ window.addEventListener('load', function(){
     .then(function(information){
         console.log(information)
         information.results.forEach(element => {
-                // if (element == 'original_name'){
-                    resultadosBusqueda.innerHTML += `
-                    <li> 
-                    <h2> ${element.original_name} </h2> 
-                    <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> 
-                    </li>`
-                //  }
-                //  else if(element == 'original_title'){
-                    //      resultadosBusqueda.innerHTML += `
-                //      <li> 
-                //          <h2> ${element.original_title} </h2> 
-                //          <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> 
-                //      </li>`
-                //  }
-                //  else{
-                    //      resultadosBusqueda.innerHTML += `
-                //      <li> 
-                //          <h2> No se encontraron resultados... </h2> `
-                //  }
+            if(element.media_type == 'movie'){
+                resultadosBusqueda.innerHTML +=`
+                <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="clsActivated: uk-transition-active; center: true">
+
+                    <ul class="uk-slider-items uk-grid">
+                        <li class="uk-width-3-4">
+                            <div class="uk-panel">
+                            <a href='detailsPeli.html?id=${element.id}&title=${element.title}&img=${element.poster_path}&gnrid=${element.genre_ids}&resumen=${element.overview}'> <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> </a>
+                                <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                                    <h3 class="uk-margin-remove">${element.title}</h3>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+        
+                
+                </div>
+                `
+            };
+            if (element.media_type == 'tv'){
+                resultadosBusqueda.innerHTML += `
             
-            
-            
+                <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="clsActivated: uk-transition-active; center: true">
+                
+                <ul class="uk-slider-items uk-grid">
+                <li class="uk-width-3-4">
+                <div class="uk-panel">
+                <a href='detailsSerie.html?id=${element.id}&title=${element.name}&img=${element.poster_path}&gnrid=${element.genre_ids}&resumen=${element.overview}'> <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> </a>
+                <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
+                <h3 class="uk-margin-remove">${element.name}</h3>
+                </div>
+                </div>
+                </li>
+                </ul>
+                
+                
+                </div>
+                `
+            };
+        
         });
-        // for (let index = 0; index < information.results.length; index++) {
-            //     const element = information.results[index];
-            
-            //}
-        })
-        .catch(function(error) {
-            console.log("Error: " + error);
-        })
-        
-        
-        
-        
-    })//NO BORRAR
-    
-                                                    // switch (element) {
-                                                    //     case 'original_name':
-                                                    //         resultadosBusqueda.innerHTML += `
-                                                    //         <li> 
-                                                    //         <h2> ${element.original_name} </h2> 
-                                                    //         <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> 
-                                                    //         </li>`
-                                                    //         break;
-                                                    //     case 'original_title':
-                                                    //         resultadosBusqueda.innerHTML += `
-                                                    //         <li> 
-                                                    //         <h2> ${element.original_title} </h2> 
-                                                    //         <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> 
-                                                    //         </li>`
-                                                    //         break;
-                                                    //     default:
-                                                    //         resultadosBusqueda.innerHTML =
-                                                    //         'No se encontraron resultados...'
-                                                    //         break;
-                                                    // }
+    });
+
+
+
+
+}) //NO BORRAR 
