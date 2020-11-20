3323cf34ba58e8f9c71fd4df671f2964ea0e7bc7
Pedro Pla <ppla2001@gmail.com>
2020-11-20 13:28:17 -0300

    arregle buscador

--- JS/resultadoBusqueda.js
+++ JS/resultadoBusqueda.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function(){
                     <ul class="uk-slider-items uk-grid">
                         <li class="uk-width-3-4">
                             <div class="uk-panel">
-                            <a href='detailsPeli.html?id=${element.id}&title=${element.title}&img=${element.poster_path}&gnrid=${element.genre_ids}&resumen=${element.overview}'> <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> </a>
+                            <a href='details.html?media_type=movie&id=${element.id}&title=${element.title}&img=${element.poster_path}&gnrid=${element.genre_ids}&resumen=${element.overview}'> <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> </a>
                                 <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
                                     <h3 class="uk-margin-remove">${element.title}</h3>
                                 </div>
@@ -38,7 +38,7 @@ window.addEventListener('load', function(){
                 <ul class="uk-slider-items uk-grid">
                 <li class="uk-width-3-4">
                 <div class="uk-panel">
-                <a href='detailsSerie.html?id=${element.id}&title=${element.name}&img=${element.poster_path}&gnrid=${element.genre_ids}&resumen=${element.overview}'> <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> </a>
+                <a href='detail.html?media_type=tv&id=${element.id}&title=${element.name}&img=${element.poster_path}&gnrid=${element.genre_ids}&resumen=${element.overview}'> <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt=""> </a>
                 <div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
                 <h3 class="uk-margin-remove">${element.name}</h3>
                 </div>
