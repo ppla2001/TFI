ba7ec24521b0d6f5404cad5756116aad736b2b3d
Melanie De Giusti <meludegiusti@Melanies-MacBook-Pro.local>
2020-11-26 22:49:53 -0300

    adite footer / mobile / iniciar sesion

---
+++ JS/inicioSesion.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function(){
+
+    if (window.sessionStorage.getItem("nombre")!= && window.sessionStorage.getItem("nombre") !=null) {
+        document.querySelector('.fede').innerHTML = window.sessionStorage.getitem("nombre")
+    }
+    document.querySelector("form.form").addEventListener("submit",function(evento){
+
+        var name = docuemnt.querySelector("input[name='nombre']").value
+        console.log(name);
+        sessionStorage.setItem("nombre", name)
+        sessionStorage.getItem("nombre")
+
+        var email = docuemnt.querySelector("input[name='email']").value
+        console.log(email);
+        sessionStorage.setItem("email", email)
+        sessionStorage.getItem("email")
+
+        var genero = docuemnt.querySelector("select[name='genero']").value
+        console.log(genero);
+        sessionStorage.setItem("genero", genero)
+        sessionStorage.getItem("genero")
+
+    })
+
+
+
+
+
+}) //NO BORRAR
